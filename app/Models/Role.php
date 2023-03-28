b1f60c78fe4ce77df5a9563de62c070c0750e4b9
Hugo <karhou@intexnetworking.com>
2023-03-28 22:04:10 +0800

    1. Add Role Blameable Columns

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -9,26 +9,5 @@ use TCG\Voyager\Tests\Database\Factories\RoleFactory;
 
 class Role extends \TCG\Voyager\Models\Role
 {
-    use Blameable, HasFactory;
-
-    protected $guarded = [];
-
-    public function users()
-    {
-        $userModel = Voyager::modelClass('User');
-
-        return $this->belongsToMany($userModel, 'user_roles')
-                    ->select(app($userModel)->getTable().'.*')
-                    ->union($this->hasMany($userModel))->getQuery();
-    }
-
-    public function permissions()
-    {
-        return $this->belongsToMany(Voyager::modelClass('Permission'));
-    }
-
-    protected static function newFactory()
-    {
-        return RoleFactory::new();
-    }
+    use Blameable;
 }
