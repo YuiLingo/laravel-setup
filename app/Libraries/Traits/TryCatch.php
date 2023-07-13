f8e6d1d3824042b42cc57a189bcb1feb84003e73
Hugo <karhou@9loop.asia>
2023-07-13 21:14:43 +0800

    1. Update TryCatch Helpers

--- app/Libraries/Traits/TryCatch.php
+++ app/Libraries/Traits/TryCatch.php
@@ -1,57 +1,37 @@
 <?php
 namespace App\Libraries\Traits;
 
+use App\Libraries\Helpers\Response;
 use Closure;
 use DB;
 use Exception;
+use Illuminate\Support\Facades\Log;
 use Throwable;
-use App\Libraries\Helpers\Response;
 
 trait TryCatch {
 
-    public function tryCatch(Closure $callback, $exception = false) {
+    public function tryCatch(Closure $callback, $exception = false, $rollback = true, $logging = null) {
 
         $response = new Response;
-
         $localTransaction = DB::transactionLevel() ? false : true;
-
-        if($localTransaction) {
-
-            DB::beginTransaction();
-
-        }
+        if ($localTransaction) {DB::beginTransaction();}
 
         try {
 
-            $result = $callback($response);
-
-            if($localTransaction) {
-
-                DB::commit();
-
-            }
-
-            $response->setData($result);
+            $response->setData($callback($response));
+            if ($localTransaction) {DB::commit();}
 
         } catch (Throwable $e) {
 
-            if($localTransaction) {
+            if ($localTransaction) {if ($rollback) {DB::rollback();}}
 
-                DB::rollback();
-
-            }
-
-            if($exception) {
-
-                throw new Exception($e->getMessage());
-
-            }
+            if ($exception) {throw new Exception($e->getMessage());}
 
             $response->setError($e->getMessage());
 
+            if (!empty($logging)) {Log::channel($logging)->error($e);}
         }
 
         return $response;
-
     }
 }
